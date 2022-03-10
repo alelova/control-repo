2b177f02f75131146c9893c47200557ac918ef7d
Alejandro <alelova@gmail.com>
2022-03-10 15:04:25 +0100

    Update r10k.pp

--- site/profile/manifests/r10k.pp
+++ site/profile/manifests/r10k.pp
@@ -3,7 +3,7 @@ class profile::r10k {
     remote => 'https://github.com/alelova/control-repo',
   }
   class {'r10k::webhook::config':
-    use_ncollective => false,
+    use_mcollective => false,
     enable_ssl => false,
   }
   class {'r10k::webhook':
