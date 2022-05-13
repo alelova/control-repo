6108bc644fdce6a16feed6abc4821499048cdd8b
Alejandro <alelova@gmail.com>
2022-05-13 15:31:17 +0200

    Update readme.pp

--- site/profile/manifests/base/readme.pp
+++ site/profile/manifests/base/readme.pp
@@ -8,6 +8,6 @@ class profile::base::readme{
   }
   file {'/etc/cron.d/apagado':
     ensure => file,
-    content => "Welcome to ${fqdn}\n",
+    source: “puppet://modules/%{module_name}/base/cron.d/apagado
   }
 }
