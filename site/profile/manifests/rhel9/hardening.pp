0d0dee34f6a7e545ebdff85bfb764f694430989d
Alejandro <alelova@gmail.com>
2023-11-18 16:59:40 +0100

    Update hardening.pp

--- site/profile/manifests/rhel9/hardening.pp
+++ site/profile/manifests/rhel9/hardening.pp
@@ -3,4 +3,10 @@ class profile::rhel9::hardening{
     ensure => file,
     content => "##File managed by puppet control-repo \nHardening server name: ${fqdn}\n",
   }
+  file_line { 'virtual_host':
+  ensure => present,
+  path   => '/root/README_hardening',
+  line   => "host_name= ${fqdn}",
+  match  => 'host_name=',
+  }
 }
