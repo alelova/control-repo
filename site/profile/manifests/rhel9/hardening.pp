3010b564d6a25a5f857aa06692d2312a0755cfd4
Alejandro <alelova@gmail.com>
2023-11-18 17:58:42 +0100

    Update hardening.pp

--- site/profile/manifests/rhel9/hardening.pp
+++ site/profile/manifests/rhel9/hardening.pp
@@ -4,17 +4,23 @@ class profile::rhel9::hardening{
     content => "##File managed by puppet control-repo \nHardening server name: ${fqdn}\n",
   }
   file_line { 'virtual_host':
-  ensure => present,
-  path   => '/root/README_host',
-  line   => "host_name= ${fqdn}",
-  match  => 'host_name=',
+    ensure => present,
+    path   => '/root/README_host',
+    line   => "host_name= ${fqdn}",
+    match  => 'host_name=',
   }
   
   ###2. Sudo config
   file_line { 'sudoers_default_pty':
-  ensure => present,
-  path   => '/etc/sudoers',
-  line   => "Defaults use_pty",
-  match  => "Defaults use",
+    ensure => present,
+    path   => '/etc/sudoers',
+    line   => "Defaults use_pty",
+    match  => "Defaults use",
+  }
+  file_line { 'sudoers_timeout':
+    ensure => present,
+    path   => '/etc/sudoers',
+    line   => "Defaults timestamp_timeout=30",
+    match  => "Defaults timestamp_timeout=",
   }
 }
