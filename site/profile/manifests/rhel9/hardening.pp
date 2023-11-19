68cad2ada718d61d45483203308ed7fbb3dee6c2
Alejandro <alelova@gmail.com>
2023-11-19 17:02:24 +0100

    Update hardening.pp

--- site/profile/manifests/rhel9/hardening.pp
+++ site/profile/manifests/rhel9/hardening.pp
@@ -3,13 +3,7 @@ class profile::rhel9::hardening{
     ensure => file,
     content => "##File managed by puppet control-repo \nHardening server name: ${fqdn}\n",
   }
-  file_line { 'virtual_host':
-    ensure => present,
-    path   => '/root/README_host',
-    line   => "host_name= ${fqdn}",
-    match  => 'host_name=',
-  }
-  
+    
   ###2. Sudo config
   file_line { 'sudoers_default_pty':
     ensure => present,
@@ -34,9 +28,43 @@ class profile::rhel9::hardening{
     content => "He leido y acepto los terminos de TFM Uned para el uso de este servidor",
   }
 
-  ###4. Password
+  ###4. Password quality
   file {'/etc/security/pwquality.conf':
     ensure => file,
     content => "minclass = 4\nminlen = 14\nretry =3",
+  }
+
+  ###5. Password expiration
+  file_line { 'pass_max_days':
+    ensure => present,
+    path   => '/etc/login.defs',
+    line   => "PASS_MAX_DAYS 45",
+    match  => "PASS_MAX_DAYS",
+  }
+  file_line { 'pass_min_days':
+    ensure => present,
+    path   => '/etc/login.defs',
+    line   => "PASS_MIN_DAYS 2",
+    match  => "PASS_MIN_DAYS",
+  }
+  file_line { 'pass_warn_age':
+    ensure => present,
+    path   => '/etc/login.defs',
+    line   => "PASS_WARN_AGE 10",
+    match  => "PASS_WARN_AGE",
+  }
+
+  ###6. Root Logins
+  file_line { 'group_required_for_su':
+    ensure => present,
+    path   => '/etc/pam.d/su',
+    line   => "auth required pam_wheel.so use_uid",
+  }
+
+  ###7. Audit records
+  file {'/etc/audit/rules.d/10_hardening.rules':
+    ensure => file,
+    content => "##File managed by puppet control-repo \n
+                -a always,exit -F arch=b64 -S chmod -F auid>=1000 -F auid!=unset -F key=perm_mod \n",
   }
 }
