714010dbdd94d7a3c4ffefc2479f991dea0adc5b
Alejandro <alelova@gmail.com>
2023-11-19 17:46:57 +0100

    Update hardening.pp with audit rules

--- site/profile/manifests/rhel9/hardening.pp
+++ site/profile/manifests/rhel9/hardening.pp
@@ -62,9 +62,10 @@ class profile::rhel9::hardening{
   }
 
   ###7. Audit records
-  file {'/etc/audit/rules.d/10_hardening.rules':
-    ensure => file,
-    content => "##File managed by puppet control-repo \n
-                -a always,exit -F arch=b64 -S chmod -F auid>=1000 -F auid!=unset -F key=perm_mod \n",
+  file_line { 'audit_changes':
+    ensure => present,
+    path   => '/etc/audit/rules.d/autit.rules',
+    line   => "-a always,exit -F arch=b64 -S chmod -F auid>=1000 -F auid!=unset -F key=perm_mod",
   }
+
 }
